c8d619d179e307dd9b3538052d390642a6288306
shankar Aeologic <shankarharbola26@gmail.com>
2020-04-07 11:12:01 +0530

    login 1

--- src/Login.java
+++ src/Login.java
@@ -6,6 +6,8 @@ public class Login {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "/home/aeologic/Downloads/chromedriver");
 		WebDriver driver = new ChromeDriver();
+		driver.get("http://dev.thespicetag.com/login");
+		
 
 	}
 

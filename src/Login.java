2b109f4f87f705d7ef44ccb597f4d972c84ad0d3
shankar Aeologic <shankarharbola26@gmail.com>
2020-04-07 11:08:46 +0530

    login

--- src/Login.java
+++ src/Login.java
@@ -1,8 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Login {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "/home/aeologic/Downloads/chromedriver");
+		WebDriver driver = new ChromeDriver();
 
 	}
 
